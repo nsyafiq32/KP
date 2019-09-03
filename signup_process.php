26004a38e7d1b236c16a49003cc50b23feb1cb59
Moch. Naufal <naufalsyafiq71@gmail.com>
2019-09-03 11:03:04 +0700

    final

--- signup_process.php
+++ signup_process.php
@@ -3,14 +3,16 @@
     
     if(isset($_POST['sign_up'])){
         $username = $_POST['username'];
-        $password = md5($_POST['password']);
-        // $password = $_POST['password'];
+        // $password = md5($_POST['password']);
+        $password = $_POST['password'];
         $pilih = $_POST['pilih'];
 
         $sql_get = mysqli_query($koneksi, "SELECT * FROM user WHERE username='$username'");
         $num_row = mysqli_num_rows($sql_get);
 
         if(isset($_POST['sign_up'])){
+            // $password = md5($_POST['password']);
+            // $password = $_POST['password'];
             if (empty($username)){
                 header("location:signupForm.php?pesan=username_belum");
             }else if(empty($password)){
@@ -19,11 +21,11 @@
                 header("location:signupForm.php?pesan=sebagai_belum");    
             }else if($num_row == 1){
                 header("location:signupForm.php?pesan=sudah_ada");
-            // }else if(strlen($password) <= 4){
-            //     header("location:signupForm.php?pesan=pass_kurang");
+            }else if(strlen($password) <= 4){
+                header("location:signupForm.php?pesan=pass_kurang");
             }else{
                 header("location:loginForm.php");
-                $query = mysqli_query($koneksi, "insert into user values('$username','$password','$pilih')");
+                $query = mysqli_query($koneksi, "insert into user values('$username',md5('$password'),'$pilih')");
             }
         }
     }
